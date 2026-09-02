045888d7734756fdf4241e79e379e6d0f6a8b8a4
kaanaydin02 <kaan@Kaan.localdomain>
2026-09-02 15:04:28 +0300

    kick off olist challenge

--- olist/data.py
+++ olist/data.py
@@ -5,22 +5,28 @@ import pandas as pd
 class Olist:
     """
     The Olist class provides methods to interact with Olist's e-commerce data.
-
-    Methods:
-        get_data():
-            Loads and returns a dictionary where keys are dataset names (e.g., 'sellers', 'orders')
-            and values are pandas DataFrames loaded from corresponding CSV files.
-
-        ping():
-            Prints "pong" to confirm the method is callable.
     """
+
     def get_data(self):
         """
         This function returns a Python dict.
-        Its keys should be 'sellers', 'orders', 'order_items' etc...
-        Its values should be pandas.DataFrames loaded from csv files
+        Its keys are 'sellers', 'orders', 'order_items' etc...
+        Its values are pandas.DataFrames loaded from csv files
         """
-        pass  # YOUR CODE HERE
+        csv_path = Path.home() / ".workintech" / "olist" / "data" / "csv"
+
+        file_names = [f.name for f in csv_path.glob("*.csv")]
+
+        key_names = [
+            f.replace("olist_", "").replace("_dataset", "").replace(".csv", "")
+            for f in file_names
+        ]
+
+        data = {}
+        for key, file_name in zip(key_names, file_names):
+            data[key] = pd.read_csv(csv_path / file_name)
+
+        return data
 
     def ping(self):
         """
