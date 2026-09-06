6eaa8fe30deae8b7bacb6d8562203a5d6bf797e8
kaanaydin02 <kaan@Kaan.localdomain>
2026-09-06 11:33:17 +0300

    Add get_training_data method

--- olist/order.py
+++ olist/order.py
@@ -82,3 +82,24 @@ class Order:
         order_items = self.data['order_items'].copy()
 
         return order_items.groupby('order_id', as_index=False).agg({'price': 'sum', 'freight_value': 'sum'})
+
+    def get_training_data(self, with_distance_seller_customer=False):
+        """
+        Returns a clean DataFrame (no NaN), with the columns:
+        order_id, wait_time, expected_wait_time, delay_vs_expected, order_status,
+        dim_is_five_star, dim_is_one_star, review_score, number_of_items,
+        number_of_sellers, price, freight_value
+        and optionally distance_seller_customer
+        """
+        training_set = (
+            self.get_wait_time()
+            .merge(self.get_review_score(), on='order_id')
+            .merge(self.get_number_items(), on='order_id')
+            .merge(self.get_number_sellers(), on='order_id')
+            .merge(self.get_price_and_freight(), on='order_id')
+        )
+
+        if with_distance_seller_customer:
+            training_set = training_set.merge(self.get_distance_seller_customer(), on='order_id')
+
+        return training_set.dropna()
