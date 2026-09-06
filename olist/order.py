3885425db6c27268af63086e75d318aee5bb0adb
kaanaydin02 <kaan@Kaan.localdomain>
2026-09-06 11:21:20 +0300

    Add get_wait_time method

--- olist/order.py
+++ olist/order.py
@@ -1,72 +1,45 @@
 import pandas as pd
 import numpy as np
-from olist.utils import haversine_distance
 from olist.data import Olist
 
 
 class Order:
-    '''
+    """
     DataFrames containing all orders as index,
     and various properties of these orders as columns
-    '''
+    """
+
     def __init__(self):
         # Assign an attribute ".data" to all new instances of Order
         self.data = Olist().get_data()
 
-    def get_wait_time(self, is_delivered=True):
+    def get_wait_time(self):
         """
         Returns a DataFrame with:
-        [order_id, wait_time, expected_wait_time, delay_vs_expected, order_status]
-        and filters out non-delivered orders unless specified
+        order_id, wait_time, expected_wait_time, delay_vs_expected, order_status
         """
-        # Hint: Within this instance method, you have access to the instance of the class Order in the variable self, as well as all its attributes
-        pass  # YOUR CODE HERE
+        orders = self.data['orders'].copy()
+        orders = orders[orders['order_status'] == 'delivered'].copy()
 
-    def get_review_score(self):
-        """
-        Returns a DataFrame with:
-        order_id, dim_is_five_star, dim_is_one_star, review_score
-        """
-        pass  # YOUR CODE HERE
+        date_columns = [
+            'order_purchase_timestamp',
+            'order_delivered_customer_date',
+            'order_estimated_delivery_date',
+        ]
+        for c in date_columns:
+            orders[c] = pd.to_datetime(orders[c])
 
-    def get_number_items(self):
-        """
-        Returns a DataFrame with:
-        order_id, number_of_items
-        """
-        pass  # YOUR CODE HERE
+        orders['wait_time'] = (
+            orders['order_delivered_customer_date'] - orders['order_purchase_timestamp']
+        ) / np.timedelta64(1, 'D')
 
-    def get_number_sellers(self):
-        """
-        Returns a DataFrame with:
-        order_id, number_of_sellers
-        """
-        pass  # YOUR CODE HERE
+        orders['expected_wait_time'] = (
+            orders['order_estimated_delivery_date'] - orders['order_purchase_timestamp']
+        ) / np.timedelta64(1, 'D')
 
-    def get_price_and_freight(self):
-        """
-        Returns a DataFrame with:
-        order_id, price, freight_value
-        """
-        pass  # YOUR CODE HERE
+        orders['delay_vs_expected'] = (
+            orders['order_delivered_customer_date'] - orders['order_estimated_delivery_date']
+        ) / np.timedelta64(1, 'D')
+        orders['delay_vs_expected'] = orders['delay_vs_expected'].apply(lambda x: max(x, 0))
 
-    # Optional
-    def get_distance_seller_customer(self):
-        """
-        Returns a DataFrame with:
-        order_id, distance_seller_customer
-        """
-        pass  # YOUR CODE HERE
-
-    def get_training_data(self,
-                          is_delivered=True,
-                          with_distance_seller_customer=False):
-        """
-        Returns a clean DataFrame (without NaN), with the all following columns:
-        ['order_id', 'wait_time', 'expected_wait_time', 'delay_vs_expected',
-        'order_status', 'dim_is_five_star', 'dim_is_one_star', 'review_score',
-        'number_of_items', 'number_of_sellers', 'price', 'freight_value',
-        'distance_seller_customer']
-        """
-        # Hint: make sure to re-use your instance methods defined above
-        pass  # YOUR CODE HERE
+        return orders[['order_id', 'wait_time', 'expected_wait_time', 'delay_vs_expected', 'order_status']]
