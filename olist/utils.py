2ce8453e7532f33812adee5d957c5c4af92d5894
kaanaydin02 <kaan@Kaan.localdomain>
2026-09-09 18:22:48 +0300

    Add return_significative_coef function

--- olist/utils.py
+++ olist/utils.py
@@ -15,3 +15,14 @@ def haversine_distance(lon1, lat1, lon2, lat2):
     c = 2 * np.arcsin(np.sqrt(a))
     km = 6371 * c
     return km
+
+
+def return_significative_coef(model, threshold=0.05):
+    """
+    Returns a Series of the model's coefficients (excluding Intercept)
+    that are statistically significant (p-value < threshold), sorted.
+    """
+    coefs = model.params[1:]
+    pvalues = model.pvalues[1:]
+    significant_coefs = coefs[pvalues < threshold]
+    return significant_coefs.sort_values()
